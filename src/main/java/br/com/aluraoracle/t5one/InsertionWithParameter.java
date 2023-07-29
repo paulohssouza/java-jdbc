57500f6b8d28f7ed28683e5031c39167ba9c97e3
phbarbudo <paulohss.souza@gmail.com>
2023-07-29 06:58:21 -0300

    Extract the insert function to a method and disable auto commit so that we can have control of the transactions made in the database

--- src/main/java/br/com/aluraoracle/t5one/InsertionWithParameter.java
+++ src/main/java/br/com/aluraoracle/t5one/InsertionWithParameter.java
@@ -4,11 +4,29 @@ import java.sql.*;
 
 public class InsertionWithParameter {
     public static void main(String[] args) throws SQLException {
-        String name = "Mouse";
-        String description = "Mouse sem fio.";
         Connection connection = ConnectionFactory.triggersConnection();
-        PreparedStatement statement = connection.prepareStatement("insert into tbproduct" +
-                "(name, description) values(?, ?)", Statement.RETURN_GENERATED_KEYS);
+        connection.setAutoCommit(false);
+        try{
+            PreparedStatement statement = connection.prepareStatement("insert into tbproduct" +
+                    "(name, description) values(?, ?)", Statement.RETURN_GENERATED_KEYS);
+            insertItem(statement, "SmartTV", "45 polegadas");
+            insertItem(statement, "Radio", "Radio a pilha");
+            insertItem(statement, "Cafeteira", "Cafeteira com cápsulas");
+            insertItem(statement, "liquidificador", "liquidificador multiprocessador");
+
+            connection.commit();
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            System.out.println("Rollback executado");
+            connection.rollback();
+        }
+
+
+        connection.close();
+    }
+
+    private static void insertItem(PreparedStatement statement, String name, String description) throws SQLException {
         statement.setString(1, name);
         statement.setString(2, description);
         statement.execute();
@@ -17,6 +35,5 @@ public class InsertionWithParameter {
             int id = resultSet.getInt(1);
             System.out.println("ID criado: " + id);
         }
-        connection.close();
     }
 }
