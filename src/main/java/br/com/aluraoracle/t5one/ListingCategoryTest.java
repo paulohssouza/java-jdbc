999f1351a5d1745cb8c20d9b01242eb11b71ba18
phbarbudo <paulohss.souza@gmail.com>
2023-07-30 11:42:38 -0300

    Implementation of a search of products by category with an n+1 querry. What becomes clear during the execution that is not a good practice, because it makes too much access to the database

--- src/main/java/br/com/aluraoracle/t5one/ListingCategoryTest.java
+++ src/main/java/br/com/aluraoracle/t5one/ListingCategoryTest.java
@@ -1,7 +1,9 @@
 package br.com.aluraoracle.t5one;
 
 import br.com.aluraoracle.t5one.dao.CategoryDAO;
+import br.com.aluraoracle.t5one.dao.ProductDAO;
 import br.com.aluraoracle.t5one.model.Category;
+import br.com.aluraoracle.t5one.model.Product;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -12,7 +14,21 @@ public class ListingCategoryTest {
         try(Connection connection = new ConnectionFactory().triggersConnection()) {
             CategoryDAO categoryDAO = new CategoryDAO(connection);
             List<Category> categoryList = categoryDAO.listing();
-            categoryList.forEach(System.out::println);
+            categoryList.forEach(
+                    category -> {
+                        System.out.println(category);
+                        try {
+                            for (Product product : new ProductDAO(connection).search(category)
+                                 ) {
+                                System.out.println(category.getName() +
+                                        " - " + product.getName());
+                            }
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+            );
+
         }
     }
 }
--- src/main/java/br/com/aluraoracle/t5one/dao/ProductDAO.java
+++ src/main/java/br/com/aluraoracle/t5one/dao/ProductDAO.java
@@ -1,5 +1,6 @@
 package br.com.aluraoracle.t5one.dao;
 
+import br.com.aluraoracle.t5one.model.Category;
 import br.com.aluraoracle.t5one.model.Product;
 
 import java.sql.*;
@@ -32,6 +33,23 @@ public class ProductDAO {
         List<Product> productList = new ArrayList<>();
         String mySql = "select id, name, description from tbproduct";
         try(PreparedStatement preparedStatement = connection.prepareStatement(mySql)) {
+            preparedStatement.execute();
+            try(ResultSet resultSet = preparedStatement.getResultSet()) {
+                while(resultSet.next()) {
+                    Product product = new Product(resultSet.getInt(1),
+                            resultSet.getNString(2), resultSet.getString(3) );
+                    productList.add(product);
+                }
+                return productList;
+            }
+        }
+    }
+
+    public List<Product> search(Category category) throws SQLException {
+        List<Product> productList = new ArrayList<>();
+        String mySql = "select id, name, description from tbproduct where category_id = ?";
+        try(PreparedStatement preparedStatement = connection.prepareStatement(mySql)) {
+            preparedStatement.setInt(1, category.getId());
             preparedStatement.execute();
             try(ResultSet resultSet = preparedStatement.getResultSet()) {
                 while(resultSet.next()) {
